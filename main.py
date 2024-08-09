8349a2306f3264fb0dae67428162c9c4c97c855b
Victor_Pierobon <victor.martineli999@gmail.com>
2024-08-09 12:35:21 -0300

    finishing (get_calories_from_image)

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import base64
 
 client = OpenAI()
 
-def main(image):
-    with open("image_path", "rb") as image:
+def get_calories_from_image(image_path):
+    with open(image_path, "rb") as image:
         base64_image = base64.b64encode(image.read()).decode('utf-8')
 
 
     response = client.chat.completions.create(
-        model="gpt-4o"
+        model="gpt-4o",
         messages=[
-                {
-                    "role":"user", 
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": "How much calories this meal have?"
-                        },
-                        {
-                            "type": "image_url",
-                            "data": "data: image/jpeg;base64," + base64_image
-                        }
-                    ]
-
-                },  
+            {
+                "role": "user",
+                "content": [
+                    {
+                    "type": "text",
+                    "text": "How many calories is in this meal?"
+                    },
+                    {
+                    "type": "image",
+                    "image": "data:image/jpg;base64," + base64_image
+                    }
+                ]
+            }  
         ],
     )
+    response_message = response.choices[0].message
+    content = response_message.content
+
+    return content
